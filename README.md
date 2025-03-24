ccc55303df5fddf295e2bd35a8c93adc8dccb86d
angezid <82150610+angezid@users.noreply.github.com>
2025-03-24 18:36:32 +0800

    bugs fixing and code refactoring

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ A vanilla JavaScript library to perform multiple autocomplete on **input**, **te
 
 ## Description
 
-Due to using RegExp as a condition checker it's highly adjustable.  
-For large suggestion array there is a `optimize` option which can significantly speed up searching.
+It design in mind to work with large arrays of suggestion, e.g. language dictionaries.  
+A `startsWith` option allow to boost searching by splitting array into smaller ones that starts with one or two characters.
 
+Due to using RegExp as a condition checker it's highly adjustable.  
 
 ## Features
 -  Pure Vanilla JavaScript
@@ -23,14 +24,14 @@ For large suggestion array there is a `optimize` option which can significantly 
 `<script src="/auto-complete.js"></script>`
 ```
 
-**Important** the `auto-complete.js` should be instantiated before an editor. Otherwise, there will be a problem with `Enter` key in FireFox. 
+**Important** the `auto-complete.js` should be instantiated before an editor. Otherwise, there will be a problem with `Enter` and `Tab` keys in FireFox. 
 
 ``` js
 new autoComplete(); // search for element with id `autocomplete`
 const instance = new autoComplete(ctx, options);
 ```
 
-The library allow to change contenteditable element without creating new instance. Can be useful for dynamically creating elements.
+The library allow to change context element without creating new instance. Can be useful for dynamically creating elements.
 ``` js
 const instance = new autoComplete(ctx, options);
 instance.reRegisterElement(selector or HTMLElement);
@@ -58,9 +59,11 @@ instance.reRegisterElement(selector or HTMLElement);
     As a trigger can be used a word or combination characters, e.g. `/(name: *)([\p{L}]*)$/iu` trigger autocomplete only after `name:`.
     
   * `optimize` {boolean} - whether to optimize the `suggestions` array to speed up searching (default is `false`).
+    **Note** that it only available with `startsWith: true` option.  
     It's create an object with keys that are (depend on a `threshold` and `caseSensitive` options) the start character(s) of array string and values that are arrays of strings starting with these keys.  
     Thus, instead of looping through whole `suggestions` array, it's search for suggestions in significantly smaller array.
     
+  * `startsWith` {boolean} - whether to search starts with (default is `false`).
   * `caseSensitive` {boolean} - whether to search case sensitive (default is `false`).
 
   * `threshold` - a number of typed characters that trigger suggestion process (default is `1`).
@@ -74,15 +77,27 @@ instance.reRegisterElement(selector or HTMLElement);
   * `listOffsetY` {number} - a vertical offset of suggestion list (default is `5`).
   * `listOffsetX` {number} - a horizontal offset of suggestion list (default is `5`).
 
-  * `filter : (results) => {}` {function} - A callback on getting suggestion results
+  * `filter : (results) => {}` {function} - A callback on getting suggestion results; 
     * `results` {object} - the array of objects containing suggestion information:
       * `text` {string} - the original suggestion string to be added to suggestion list as list item text content
       * `value` {string} - the suggestion string prepared for comparison (depend on `caseSensitive` option)
       * `query` {string} - the query string prepared for comparison (depend on `caseSensitive` option)
       * `startIndex` {number} - the start index of this `query` substring in this `value` string
+   
+  * `sort` {boolean} - whether to sort a suggestion list by ascending order of the `startIndex` and place exact match as first item (default is `false`).
+    It only make sense with `startsWith:  false`.
+    The sorting performs after custom `filter()` callback, if any, before building the suggestion list.
         
-  * `sort` {boolean} - whether to sort a suggestion list by ascending order of the `startIndex` (default is `false`).
-    sorting is performs after custom `filter` callback, if any, before show a suggestion list.
+  * `listItem : (element, data) => {}` {function} - A callback on creation of suggestion list item:
+    * `element` {HTMLelement} - a list item element with necessary attributes
+    * `data` {object} - an object containing suggestion information:
+      * `text` {string} - the original suggestion string to be added to suggestion list as list item text content
+      * `value` {string} - the suggestion string prepared for comparison (depend on `caseSensitive` option)
+      * `query` {string} - the query string prepared for comparison (depend on `caseSensitive` option)
+      * `startIndex` {number} - the start index of this `query` substring in this `value` string
+    The value of attribute 'data-text' can be safely modify (value is a typing replacer), but don't change 'data-query'.  
+    The highlighting performs just before this callback, if`highlight: true`.
+
     
   * `debug` {boolean} - logs to the console internal messages (default is `false`).
   
--- src/auto-complete.js
+++ src/auto-complete.js
@@ -30,18 +30,18 @@
 			return optimize(array);
 		}
 
-		const name = 'auto-complete',
+		const name = 'autocomplete',
 			libName = 'auto-complete.js';
 
-		let element,
+		let context,
 			queryRegex,
 			regexSource,
+			caretCoords,
 			listbox,
 			listSelector,
 			isContentEditable,
 			isInput,
-			caretRect,
-			itemLength = 0,
+			itemsLength = 0,
 			selectedIndex = 0;
 
 		const opt = Object.assign({}, {
@@ -64,16 +64,16 @@
 			//filter : () => {},
 			debug : false,
 		}, this.options);
-		
+
 		listSelector = `${opt.listTag}.${opt.listClass}`;
 		queryRegex = (opt.regex instanceof RegExp) ? opt.regex : regExpCreator.create(opt, libName);
 		log(libName + ': query RegExp - /' + queryRegex.source + '/' + queryRegex.flags);
 
-		element = registerElement(this.ctx);
+		context = registerElement(this.ctx);
 		createListbox();
 		registerEvents();
 		
-		if (opt.optimize) {
+		if (opt.optimize && opt.startsWith) {
 			opt.suggestions = optimize(opt.suggestions);
 		}
 
@@ -89,8 +89,7 @@
 		}
 
 		function createListbox() {
-			listbox = createElement(opt.listTag, opt.listClass);
-			document.body.appendChild(listbox);
+			listbox = createElement(document.body, opt.listTag, opt.listClass);
 			addEvent(listbox, 'mousedown', (e) => e.preventDefault());
 			addEvent(listbox, 'click', listItemClick);
 		}
@@ -99,14 +98,18 @@
 			replaceQuery(e.target.closest(opt.listItemTag));
 		}
 
-		function createElement(name, klass) {
+		function createElement(parent, name, klass, content) {
 			let elem = document.createElement(name);
+
 			if (klass) elem.setAttribute('class', klass);
+			if (content) elem.textContent = content;
+
+			parent.appendChild(elem);
 			return elem;
 		}
 
 		function registerEvents() {
-			addEvent(window, 'load', hideLists());    // FireFox displays all lists on load
+			addEvent(window, 'load', hideLists);    // FireFox displays all lists on load
 			addEvent(window, 'resize', hide);
 			addEvent(document, 'click', outsideClick);
 		}
@@ -116,7 +119,7 @@
 				document.querySelectorAll(listSelector).forEach(elem => { elem.style.display = 'none'; });
 			}, 20);
 		}
-		
+
 		function outsideClick(e) {
 			if ( !listbox.contains(e.target) && !listbox.contains(e.target)) hide();
 		}
@@ -126,19 +129,29 @@
 		}
 
 		function process() {
+			caretCoords = null;
 			const obj = getQuery();
 
-			if (obj && obj.query.length >= opt.threshold && caretRect) {
+			if (obj && obj.query.length >= opt.threshold && caretCoords) {
 				let array = getSuggestions(obj);
 
-				if (typeof opt.filter === 'function' && array.length) array = opt.filter(array);
+				if (typeof opt.filter === 'function' && array.length) {
+					array = opt.filter(array) || array; // in case of modification of existing array
+				}
 
-				itemLength = array.length;
+				itemsLength = array.length;
 
-				if (itemLength) {
+				if (itemsLength) {
 					if ( !opt.startsWith && opt.sort) {
 						// sorts by the priority of query substring is more closer to the beginning of suggestion string
 						array.sort((a, b) => a.startIndex - b.startIndex);
+
+						const index = array.findIndex(a => a.startIndex === 0 && a.query === a.value);
+						if (index > 0) {
+							const temp = array[0];
+							array[0] = array[index];
+							array[index] = temp;
+						}
 					}
 					show(array);
 					return;
@@ -148,16 +161,15 @@
 		}
 
 		function getQuery() {
-			caretRect = null;
 			let text = '';
 
 			if (isContentEditable) {
-				text = getText();
+				text = contentEditable.getText(context);
+				caretCoords = contentEditable.caretCoordinates;
 
 			} else {
-				const lastIndex = element.selectionStart;
-				text = element.value.substr(0, lastIndex);
-				caretRect = getCaretCoordinates(element, lastIndex);
+				text = textarea.getText(context);
+				caretCoords = textarea.caretCoordinates;
 			}
 
 			const rm = queryRegex.exec(text);
@@ -170,16 +182,48 @@
 			return null;
 		}
 
-		// taken from 'codejar.js' and adapted for this library
-		function getText() {
-			const rng = getSelection().getRangeAt(0),
-				range = document.createRange();
+		function show(list) {
+			listbox.innerHTML = '';
+			const array = [],
+				custom = typeof opt.listItem === 'function';
+			let highlight = opt.highlight;
 
-			caretRect = rng.getBoundingClientRect();
+			list.forEach((data, i) => {
+				let text = data.text;
+				const elem = createElement(listbox, opt.listItemTag, opt.listItemClass, text);
+				elem.setAttribute('data-query', data.query);
+				elem.setAttribute('data-text', text);
 
-			range.selectNodeContents(element);
-			range.setEnd(rng.startContainer, rng.startOffset);
-			return range.toString();
+				if (highlight) {
+					const start = data.startIndex,
+						end = start + data.query.length;
+
+					elem.textContent = start > 0 ? text.substr(0, start) : '';
+					createElement(elem, 'mark', null, text.substring(start, end));
+
+					if (end < text.length) {
+						elem.appendChild(document.createTextNode(text.substr(end)));
+					}
+				}
+
+				if (custom) {
+					opt.listItem(elem, data);
+				}
+			});
+
+			// must be set before calling 'getListPlacement()'
+			listbox.style.display = 'block';
+
+			const rect = getListPlacement();
+			listbox.style.top = rect.top + 'px';
+			listbox.style.left = rect.left + 'px';
+			listbox.scrollTop = 0;
+			selectedIndex = -1;
+		}
+
+		function hide() {
+			listbox.innerHTML = '';
+			listbox.style.display = 'none';
 		}
 
 		function navigateList(e) {
@@ -208,12 +252,12 @@
 		}
 
 		function next() {
-			selectedIndex = selectedIndex >= itemLength - 1 ? 0 : selectedIndex + 1;
+			selectedIndex = selectedIndex >= itemsLength - 1 ? 0 : selectedIndex + 1;
 			update();
 		}
 
 		function previous() {
-			selectedIndex = selectedIndex <= 0 ? itemLength - 1 : selectedIndex - 1;
+			selectedIndex = selectedIndex <= 0 ? itemsLength - 1 : selectedIndex - 1;
 			update();
 		}
 
@@ -231,52 +275,6 @@
 			}
 		}
 
-		function show(list) {
-			listbox.innerHTML = '';
-			
-			list.forEach((obj, i) => {
-				const text = obj.text,
-					start = obj.startIndex,
-					end = start + obj.query.length,
-					elem = createElement(opt.listItemTag, opt.listItemClass);
-
-				elem.setAttribute('data-query', obj.query);
-				elem.setAttribute('data-text', text);
-
-				if (opt.highlight) {
-					if (start > 0) {
-						elem.textContent = text.substr(0, start);
-					}
-
-					const mark = createElement('mark');
-					mark.textContent = text.substring(start, end);
-					elem.appendChild(mark);
-
-					if (end < text.length) {
-						elem.appendChild(document.createTextNode(text.substr(end)));
-					}
-
-				} else {
-					elem.textContent = text;
-				}
-
-				listbox.appendChild(elem);
-			});
-			listbox.style.display = 'block';    // must be set before calling 'getListPlacement()'
-
-			const rect = getListPlacement();
-			listbox.style.top = rect.top + 'px';
-			listbox.style.left = rect.left + 'px';
-			listbox.scrollTop = 0;
-			selectedIndex = -1;
-		}
-
-		function hide() {
-			if ( !listbox) return; 
-			listbox.innerHTML = '';
-			listbox.style.display = 'none';
-		}
-
 		function optimize(suggestions) {
 			const obj = {};
 
@@ -291,8 +289,9 @@
 		}
 
 		function getKey(str) {
-			let key = '';
-			for (let i = 0; i < opt.threshold; i++) {
+			let key = '',
+				num = Math.min(opt.threshold, 2);
+			for (let i = 0; i < num; i++) {
 				key += str.charAt(i);
 			}
 			return opt.caseSensitive ? key : key.toLowerCase();
@@ -312,7 +311,7 @@
 
 			let results = [],
 				count = 0,
-				text, str, index, success;
+				text, str, index;
 
 			if ( !array) {
 				log(libName + ': Suggestion array is undefined for ', obj.query);
@@ -323,15 +322,8 @@
 				text = array[i];
 				str = opt.caseSensitive ? text : text.toLowerCase();
 				index = str.indexOf(query);
-				
-				if (startsWith) {
-					success = index === 0 && str.length > queryLen;
 
-				} else {
-					success = index > 0 || index === 0 && str.length > queryLen;
-				}
-
-				if (success) {
+				if (startsWith ? index === 0 : index >= 0) {
 					results.push({ text : text, value : str, query : query, startIndex : index });
 
 					if (++count >= opt.maxResults) break;
@@ -351,74 +343,20 @@
 				right = offsetX + window.innerWidth - 20,
 				bottom = offsetY + window.innerHeight - 20;
 
-			let top = caretRect.top + caretRect.height + offsetY,
-				left = caretRect.left + offsetX;
+			let top = caretCoords.top + caretCoords.height + offsetY,
+				left = caretCoords.left + offsetX;
+
+			if (top + rect.height > bottom) {
+				top = top - rect.height - caretCoords.height - listY;
+
+			} else top += listY;
 
 			if (left + rect.width > right) {
 				left = left - rect.width - listX;
 
 			} else left += listX;
 
-			if (top + rect.height > bottom) {
-				top = top - rect.height - caretRect.height - listY;
-
-			} else top += listY;
-
 			return { top : top, left : left };
-		}
-
-		// from https://github.com/component/textarea-caret-position; undergoes refactoring, bugs fixing and adaptation
-		function getCaretCoordinates(elem, lastIndex) {
-			const rect = elem.getBoundingClientRect(),
-				text = elem.value,
-				len = text.length,
-				content = text.substring(0, lastIndex),
-				style = window.getComputedStyle(elem),
-				div = document.createElement('div'),
-				span = document.createElement('span');
-
-			div.textContent = isInput ? content.replace(/\s/g, '\u00a0') : content;
-			span.textContent = text.substr(lastIndex, len - lastIndex > 50 ? 50 : len) || '.';
-			div.appendChild(span);
-			document.body.appendChild(div);
-
-			const properties = [
-				'direction', 'boxSizing',
-				'textAlign', 'textAlignLast', 'textTransform', 'textIndent',
-				'letterSpacing', 'wordSpacing', 'wordBreak',
-				'overflowX', 'overflowY',
-				'tabSize'
-			];
-
-			properties.forEach(prop => {
-				div.style[prop] = style.getPropertyValue(prop);
-			});
-
-			const props = {
-				width : style.width,
-				wordWrap : "normal",
-				whiteSpace : "pre-wrap",
-			};
-
-			Object.assign(div.style, {
-				position : "absolute",
-				visibility : "hidden",
-				top : rect.top + "px",
-				left : rect.left + "px",
-				//height : style.height,
-				font : style.font,
-				padding : style.padding,
-				border : style.border,
-			}, ( !isInput ? props : {}));
-
-			const height = span.getBoundingClientRect().height,
-				offsetY = isInput ? (elem.clientHeight - height) / 2 : 0,
-				top = rect.top + span.offsetTop - (elem.scrollHeight > elem.clientHeight ? elem.scrollTop : 0) + parseInt(style.borderTopWidth) + offsetY,
-				left = rect.left + span.offsetLeft - (elem.scrollWidth > elem.clientWidth ? elem.scrollLeft : 0) + parseInt(style.borderLeftWidth) - 1;
-
-			document.body.removeChild(div);
-
-			return { top, left, height };
 		}
 
 		function replaceQuery(elem) {
@@ -428,73 +366,145 @@
 			if ( !query) return;
 
 			if (isContentEditable) {
-				const len = getText().length;
+				contentEditable.replace(context, query, suggestion);
 
-				select(element, len - query.length, len);
+			} else {
+				textarea.replace(context, query, suggestion);
+			}
+			hide();
+		}
+
+		const contentEditable = {
+			caretCoordinates : null,
+
+			// taken from 'codejar.js' and adapted for this library
+			getText: function(elem) {
+				const rng = getSelection().getRangeAt(0),
+					range = document.createRange();
+
+				this.caretCoordinates = rng.getBoundingClientRect();
+
+				range.selectNodeContents(elem);
+				range.setEnd(rng.startContainer, rng.startOffset);
+				return range.toString();
+			},
+
+			replace: function(elem, query, suggestion) {
+				const len = this.getText(elem).length;
+
+				this.select(elem, len - query.length, len);
 
 				const text = suggestion.replace(/[<>&"']/g, m => {
 					return m === '<' ? '&lt;' : m === '>' ? '&gt;' : m === '&' ? '&amp;' : m === '"' ? '&quot;' : '&#039;';
 				});
 				document.execCommand('insertHTML', false, text);
+			},
 
-			} else {
-				const value = element.value,
-					index = element.selectionStart,
-					queryIndex = index - query.length;
+			select : function(elem, start, end) {
+				const selection = getSelection();
+				let startNode,
+					endNode,
+					startOffset = 0,
+					endOffset = 0,
+					previous = 0,
+					node;
 
-				element.value = value.substr(0, queryIndex) + suggestion + value.substr(index);
-				element.selectionStart = element.selectionEnd = queryIndex + suggestion.length; // set cursor at the end of suggestion
-			}
-			hide();
-		}
+				const iterator = document.createNodeIterator(elem, NodeFilter.SHOW_TEXT, () => NodeFilter.FILTER_ACCEPT);
+				while (node = iterator.nextNode()) {
+					if (node.nodeValue) {
+						const current = previous + node.nodeValue.length;
 
-		// taken from 'codejar.js' and adapted for this library
-		function select(element, start, end) {
-			const selection = getSelection(),
-				stack = [];
-			let startNode,
-				endNode,
-				startOffset = 0,
-				endOffset = 0,
-				current = 0,
-				elem = element.firstChild;
-
-			while (elem) {
-				if (elem.nodeType === Node.TEXT_NODE) {
-					const len = (elem.nodeValue || '').length;
-					
-					if (current + len >= start && start >= current) {
-						if ( !startNode) {
-							startNode = elem;
-							startOffset = start - current;
+						if ( !startNode && current > start) {
+							startNode = node;
+							startOffset = start - previous;
 						}
-						if (current + len >= end && end >= current) {
-							endNode = elem;
-							endOffset = end - current;
+
+						if ( !endNode && current >= end) {
+							endNode = node;
+							endOffset = end - previous;
 							break;
 						}
+						previous = current;
 					}
-					current += len;
 				}
+				if (startNode && endNode) {
+					selection.setBaseAndExtent(startNode, startOffset, endNode, endOffset);
+				}
+			}
+		};
 
-				if (elem.nextSibling) stack.push(elem.nextSibling);
-				if (elem.firstChild) stack.push(elem.firstChild);
-				elem = stack.pop();
+		const textarea = {
+			caretCoordinates : null,
+
+			getText : function(elem) {
+				const lastIndex = elem.selectionStart,
+					text = elem.value.substr(0, lastIndex);
+				this.caretCoordinates = this.getCaretCoordinates(elem, lastIndex);
+				return text;
+			},
+
+			replace: function(elem, query, suggestion) {
+				const value = elem.value,
+					index = elem.selectionStart,
+					queryIndex = index - query.length;
+
+				elem.value = value.substr(0, queryIndex) + suggestion + value.substr(index);
+				// set cursor at the end of suggestion
+				elem.selectionStart = elem.selectionEnd = queryIndex + suggestion.length;
+			},
+
+			// from https://github.com/component/textarea-caret-position; undergoes refactoring, bugs fixing and adaptation
+			getCaretCoordinates: function(elem, lastIndex) {
+				const rect = elem.getBoundingClientRect(),
+					text = elem.value,
+					len = text.length,
+					content = text.substring(0, lastIndex),
+					style = window.getComputedStyle(elem),
+
+					div = createElement(document.body, 'div', null, isInput ? content.replace(/\s/g, '\u00a0') : content),
+					span = createElement(div, 'span', null, text.substr(lastIndex, len - lastIndex > 50 ? 50 : len) || '.');
+
+				const properties = [
+					'direction', 'boxSizing',
+					'textAlign', 'textAlignLast', 'textTransform', 'textIndent',
+					'letterSpacing', 'wordSpacing', 'wordBreak',
+					'overflowX', 'overflowY',
+					'tabSize'
+				];
+
+				properties.forEach(prop => {
+					div.style[prop] = style.getPropertyValue(prop);
+				});
+
+				const props = {
+					width : style.width,
+					wordWrap : "normal",
+					whiteSpace : "pre-wrap",
+				};
+
+				Object.assign(div.style, {
+					position : "absolute",
+					visibility : "hidden",
+					top : rect.top + "px",
+					left : rect.left + "px",
+					font : style.font,
+					padding : style.padding,
+					border : style.border,
+				}, ( !isInput ? props : {}));
+
+				const height = span.getBoundingClientRect().height,
+					offsetY = isInput ? (elem.clientHeight - height) / 2 : 0,
+					top = rect.top + span.offsetTop - (elem.scrollHeight > elem.clientHeight ? elem.scrollTop : 0) + parseInt(style.borderTopWidth) + offsetY,
+					left = rect.left + span.offsetLeft - (elem.scrollWidth > elem.clientWidth ? elem.scrollLeft : 0) + parseInt(style.borderLeftWidth) - 1;
+
+				document.body.removeChild(div);
+
+				return { top, left, height };
 			}
-			if ( !startNode) {
-				startNode = element;
-				startOffset = element.childNodes.length;
-			}
-			if ( !endNode) {
-				endNode = element;
-				endOffset = element.childNodes.length;
-			}
-			selection.setBaseAndExtent(startNode, startOffset, endNode, endOffset);
-		}
+		};
 
 		function getSelection() {
-			try { return DocumentOrShadowRoot.getSelection(); } catch (e) { }
-			return window.getSelection();
+			return context.getRootNode().getSelection();
 		}
 
 		function debounce(callback, duration) {
@@ -512,16 +522,17 @@
 		}
 
 		function removeElementEvents() {
-			if (element) {
-				remove(element, 'input', onInput);
-				remove(element, 'blur', hide);
-				remove(element, 'keydown', navigateList);
+			if (context) {
+				remove(context, 'input', onInput);
+				remove(context, 'blur', hide);
+				remove(context, 'keydown', navigateList);
 			}
 		}
 
 		function removeEvents() {
 			remove(document, 'click', outsideClick);
 			remove(window, 'resize', hide);
+			remove(window, 'load', hideLists);
 
 			if (listbox) document.body.removeChild(listbox);
 		}
@@ -533,14 +544,6 @@
 		function remove(elem, type, fn) {
 			elem.removeEventListener(type, fn);
 		}
-	}
-
-	function distinct(array) {
-		const result = [];
-		array.forEach(item => {
-			if ( !result.includes(item)) result.push(item);
-		});
-		return result;
 	}
 
 	const regExpCreator = {
@@ -577,10 +580,18 @@
 		preprocess : function(value) {
 			if (value && value.length) {
 				let array = this.split(value);
-				array = distinct(array);
+				array = this.distinct(array);
 				return array.map(str => str.length > 4 || /\\[wdsWDSnt]/.test(str) ? str : this.escapeCharSet(str)).join('');
 			}
 			return '';
+		},
+
+		distinct : function(array) {
+			const result = [];
+			array.forEach(item => {
+				if ( !result.includes(item)) result.push(item);
+			});
+			return result;
 		},
 
 		escapeCharSet : function(str) {
